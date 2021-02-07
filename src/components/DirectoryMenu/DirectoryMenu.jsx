bebc684cc06ba782b4426d5c5a740299d9b77888
eliram9 <eliram9@gmail.com>
2021-02-07 02:14:57 -0500

    style ansd images

--- src/components/DirectoryMenu/DirectoryMenu.jsx
+++ src/components/DirectoryMenu/DirectoryMenu.jsx
@@ -12,28 +12,28 @@ class DirectoryMenu extends React.Component {
             sections: [
                 {
                     title: 'hats',
-                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                    imageUrl: 'https://images.pexels.com/photos/1115700/pexels-photo-1115700.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
                     id: uuid()
                 },
                 {
                     title: 'jackets',
-                    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                    imageUrl: 'https://images.unsplash.com/photo-1425100599170-85ec4f00a6ee?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80',
                     id: uuid()
                 },
                 {
                     title: 'sneakers',
-                    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                    imageUrl: 'https://images.unsplash.com/photo-1597248881519-db089d3744a5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80',
                     id: uuid()
                 },
                 {
                     title: 'womens',
-                    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                    imageUrl: 'https://images.pexels.com/photos/2036646/pexels-photo-2036646.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=1000',
                     size: 'large',
                     id: uuid()
                 },
                 {
                     title: 'mens',
-                    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                    imageUrl: 'https://images.pexels.com/photos/4096982/pexels-photo-4096982.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
                     size: 'large',
                     id: uuid()
                 }
@@ -44,10 +44,11 @@ class DirectoryMenu extends React.Component {
     render() {
         return (
         <div className='directory_menu'>
-            {this.state.sections.map(({ title, id, imageUrl }) => (
+            {this.state.sections.map(({ title, id, imageUrl, size }) => (
                 <MenuItem key={id} 
                           title={title} 
                           image={imageUrl}
+                          size={size}
                 />
             ))}
         </div>
--- src/components/menuItem/MenuItem.jsx
+++ src/components/menuItem/MenuItem.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 import './MenuItem.scss';
 
-const MenuItem = ({ title, image }) => (
-    <div className="menu_item" style={{ backgroundImage: `url(${image})` }}>
+const MenuItem = ({ title, image, size }) => (
+    <div className={`${size} menu_item`} style={{ backgroundImage: `url(${image})` }}>
         <div className='content'>
             <h1 className='title'>{title.toUpperCase()}</h1>
             <span className='subtitle'>SHOP NOW</span>
