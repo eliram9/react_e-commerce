046af0bc7a55aa5d24b9ad2f6538c7bfb98e39fd
eliram9 <eliram9@gmail.com>
2021-02-10 21:49:07 -0500

    more style for image sections

--- src/components/menuItem/MenuItem.jsx
+++ src/components/menuItem/MenuItem.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 import './MenuItem.scss';
 
 const MenuItem = ({ title, image, size }) => (
-    <div className={`${size} menu_item`} style={{ backgroundImage: `url(${image})` }}>
+    <div className={`${size} menu_item`}>
+        <div className="background_image" style={{ backgroundImage: `url(${image})` }} />
         <div className='content'>
             <h1 className='title'>{title.toUpperCase()}</h1>
             <span className='subtitle'>SHOP NOW</span>
