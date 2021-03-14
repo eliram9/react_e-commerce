c736d27fc0a92ed69dcb56d36c668e4df203caea
eliram9 <eliram9@gmail.com>
2021-03-14 03:42:04 -0400

    css header cosmetics

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,26 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import Logo from '../../assets/grail.svg'
+import Logo from '../../assets/grail.png'
 import './Header.scss';
 
 const Header = () => {
     return (  
-        // <div className='header'>
-        //     <Link className="logo_container" to="/">
-        //         <Logo className="logo" />
-        //     </Link>
-        //     <div className="options">
-        //         <Link className="option" to="/shop">SHOP</Link>
-        //         <Link className="option" to="/contact">CONTACT</Link>
-        //     </div>
-        // </div>
         <div>
             <nav className="header">
                 <div className="container">
                     <Link className="logo_container" to="/">
-                        <img src={Logo} alt="logo-image" width="60" style={{ filter: 'grayscale(.8)', marginRight: '10px' }} /> 
-                        HOLY GRAIL
+                        <img src={Logo} 
+                            alt="website_logo" 
+                            width="26" 
+                            height="26" 
+                            className="d-inline-block align-top" 
+                            style={{ filter: 'grayscale(.7)', marginRight: '10px' }} 
+                        />
+                            HOLY GRAIL
                     </Link>
                 </div>
                 <div className="options">
